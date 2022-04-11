e86bc1a44ded5fa359ac477427e6014562f484dd
ThePetaVerse <98707462+ThePetaVerse@users.noreply.github.com>
2022-04-11 18:33:33 +0100

    Update KennelJs.js

--- KennelJs.js
+++ KennelJs.js
@@ -1,6 +1,5 @@
-
 const Submitted = document.getElementById("kennellookup")
-Submitted.onclick = function KennelSearch() {
+document.getElementById("kennellookup").onclick = function KennelSearch() {
     let Knumber = document.getElementById("kennelnumber").value;
     let text;
     if (isNaN(Knumber) || Knumber < 1 || Knumber > 5) {
