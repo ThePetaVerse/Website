d14ad5c4b24b0477fc5cfe48be76839f610e8f89
ThePetaVerse <98707462+ThePetaVerse@users.noreply.github.com>
2022-04-11 18:23:06 +0100

    Add files via upload

---
+++ KennelJs.js
@@ -0,0 +1,12 @@
+
+const Submitted = document.getElementById("kennellookup")
+function KennelSearch() {
+    let Knumber = document.getElementById("kennelnumber").value;
+    let text;
+    if (isNaN(Knumber) || Knumber < 1 || Knumber > 5) {
+        text = "Invalid";
+    } else {
+        text = "Valid";
+    }
+    document.getElementById("test").innerHTML = text;
+}
